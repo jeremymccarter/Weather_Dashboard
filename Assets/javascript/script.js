e5f9515af85c57f2370083bdc227fd8c4efcaafe
dirtnasty jones <jeremymccarter27@gmail.com>
2023-01-25 22:03:46 -0800

    fixed five day forecast, working on local storage

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -37,17 +37,67 @@ function runApi() {
             })
             .then(function (data) {
                 console.log(data);
+
+             /*   function displayFunction() {
+                    var x = document.querySelector("fivedayclass");
+                    if (x.style.display === "none") {
+                      x.style.display = "block";
+                    } else {
+                      x.style.display = "none";
+                    }
+                  }
+*/
+
+           // document.getElementById("icon0").innerHTML="<img src='"+"http://openweathermap.org/img/wn/"+data.list[0].weather.icon+".png'>"
+           // document.getElementById("temperature0").innerHTML=data.list[0].main.temp+" F"
+           // document.getElementById("humidity0").innerHTML=data.list[0].main.humidity+" %"
+           // document.getElementById("windspeed0").innerHTML=data.list[0].wind.speed+" mph"
+
+            
+         /*   document.getElementById("icon1").innerHTML="<img src='"+"http://openweathermap.org/img/wn/"+data.weather[0].icon+".png'>"
+            document.getElementById("temperature1").innerHTML=data.main.temp+" F"
+            document.getElementById("humidity1").innerHTML=data.main.humidity+" %"
+            document.getElementById("windspeed1").innerHTML=data.wind+" mph"
+
+
+            document.getElementById("icon2").innerHTML="<img src='"+"http://openweathermap.org/img/wn/"+data.weather[0].icon+".png'>"
+            document.getElementById("temperature2").innerHTML=data.main.temp+" F"
+            document.getElementById("humidity2").innerHTML=data.main.humidity+" %"
+            document.getElementById("windspeed2").innerHTML=data.wind.speed+" mph"
+
+           
+            document.getElementById("icon3").innerHTML="<img src='"+"http://openweathermap.org/img/wn/"+data.weather[0].icon+".png'>"
+            document.getElementById("temperature3").innerHTML=data.main.temp+" F"
+            document.getElementById("humidity3").innerHTML=data.main.humidity+" %"
+            document.getElementById("windspeed3").innerHTML=data.wind.speed+" mph"
+
+            
+            document.getElementById("icon4").innerHTML="<img src='"+"http://openweathermap.org/img/wn/"+data.weather[0].icon+".png'>"
+            document.getElementById("temperature4").innerHTML=data.main.temp+" F"
+            document.getElementById("humidity4").innerHTML=data.main.humidity+" %"
+            document.getElementById("windspeed4").innerHTML=data.wind.speed+" mph"
+*/
+
              for (let index = 0; index < 40; index+=8) {
                 var output = "<div>"
-                output+=data.list[index].dt_txt
+                var date = data.list[index].dt_txt
+                var year = date.substring(0,4)
+                var month = date.substring(5,7)
+                var day = date.substring(8,10)
+                var dateString = month+"/"+day+"/"+year
+                output+="<p>"+dateString+"</p>"
+                output+="<p><img src='http://openweathermap.org/img/wn/"+data.list[index].weather[0].icon+".png'></p>"
+                output+="<p> Temperature: "+data.list[index].main.temp+" F </p>"
+                output+="<p> Humidity: "+data.list[index].main.humidity+" % </p>"
+                output+="<p> Wind Speed: "+data.list[index].wind.speed+" mph </p>"
                 output+="</div>"
-                document.getElementById("fiveday").innerHTML+=output
+                document.getElementById("fivedaydisplay").innerHTML+=output
              }  
             });
 
-
-        });
-}
+        })
+       
+    }
 
 
 document.getElementById("searchbutton").addEventListener("click",runApi)
